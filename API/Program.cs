1ad00b3d8dc6d5ebe73c13d0c03f5377d672781e
Laurent-Moua <moual8@matc.edu>
2026-09-20 10:18:12 -0700

    merg conflicts

--- API/Program.cs
+++ API/Program.cs
@@ -6,16 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
 }
 
 //app.UseHttpsRedirection();
